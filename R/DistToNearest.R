1c18331e6deb31bc635aef84853ab7d16653adc6
Mohamed Uduman <mohamed.uduman@gmail.com>
2015-02-13 14:37:42 -0500

    Minor changes

--- R/DistToNearest.R
+++ R/DistToNearest.R
@@ -13,16 +13,16 @@
 # @return  a list containing the substitution (subs) and mutability (mut) models
 #' @export
 loadModel <- function(model) {
-    if (model == "hs5f") { data_file = "HS5F_Targeting.RData" }
-    else if (model == "m3n") { data_file = "MTri_Targeting.RData" }
-    else { stop("Are you sure you know what you're doing?\n") }
-
-    tmp_env <- new.env()
-    load(system.file("extdata", data_file, package="shm"), envir=tmp_env)
-    Targeting <- get("Targeting", envir=tmp_env)
-    rm(tmp_env)
-
-    return(list(subs=Targeting[["Substitution"]], mut=Targeting[["Mutability"]]))
+  if (model == "hs5f") { data_file = "HS5F_Targeting.RData" }
+  else if (model == "m3n") { data_file = "MTri_Targeting.RData" }
+  else { stop("Are you sure you know what you're doing?\n") }
+  
+  tmp_env <- new.env()
+  load(system.file("extdata", data_file, package="shm"), envir=tmp_env)
+  Targeting <- get("Targeting", envir=tmp_env)
+  rm(tmp_env)
+  
+  return(list(subs=Targeting[["Substitution"]], mut=Targeting[["Mutability"]]))
 }
 
 
@@ -50,23 +50,23 @@ loadModel <- function(model) {
 #' dist_seq_fast(seq1, seq2, model_data[["subs"]], model_data[["mut"]])
 #' @export
 dist_seq_fast <- function(seq1, seq2, subs, mut){
-    #Compute distance only on fivemers that have mutations
-    fivemersWithMu <- substr(seq1,3,3)!=substr(seq2,3,3)
-    fivemersWithNonNuc <- ( !is.na(match(substr(seq1,3,3),c("A","C","G","T"))) & !is.na(match(substr(seq2,3,3),c("A","C","G","T"))) )
-    seq1 <- seq1[fivemersWithMu & fivemersWithNonNuc]
-    seq2 <- seq2[fivemersWithMu & fivemersWithNonNuc]
-    a <- tryCatch({
-        if(length(seq1)==1){
-            seq1_to_seq2 <- subs[substr(seq2,3,3),seq1] * mut[seq1]
-            seq2_to_seq1 <- subs[substr(seq1,3,3),seq2] * mut[seq2]
-        }else{
-            seq1_to_seq2 <- sum( diag(subs[substr(seq2,3,3),seq1]) *  mut[seq1] )
-            seq2_to_seq1 <- sum( diag(subs[substr(seq1,3,3),seq2]) *  mut[seq2] )
-        }
-        return( mean(c(seq1_to_seq2, seq2_to_seq1)) )
-    },error = function(e){
-        return(NA)
-    })
+  #Compute distance only on fivemers that have mutations
+  fivemersWithMu <- substr(seq1,3,3)!=substr(seq2,3,3)
+  fivemersWithNonNuc <- ( !is.na(match(substr(seq1,3,3),c("A","C","G","T"))) & !is.na(match(substr(seq2,3,3),c("A","C","G","T"))) )
+  seq1 <- seq1[fivemersWithMu & fivemersWithNonNuc]
+  seq2 <- seq2[fivemersWithMu & fivemersWithNonNuc]
+  a <- tryCatch({
+    if(length(seq1)==1){
+      seq1_to_seq2 <- subs[substr(seq2,3,3),seq1] * mut[seq1]
+      seq2_to_seq1 <- subs[substr(seq1,3,3),seq2] * mut[seq2]
+    }else{
+      seq1_to_seq2 <- sum( diag(subs[substr(seq2,3,3),seq1]) *  mut[seq1] )
+      seq2_to_seq1 <- sum( diag(subs[substr(seq1,3,3),seq2]) *  mut[seq2] )
+    }
+    return( mean(c(seq1_to_seq2, seq2_to_seq1)) )
+  },error = function(e){
+    return(NA)
+  })
 }
 
 
@@ -78,23 +78,23 @@ dist_seq_fast <- function(seq1, seq2, subs, mut){
 # @return  A vector of distances to the closest sequence.
 #' @export
 getDistanceToClosest <- function(arrJunctions, subs, mut){
-
+  
   #Initialize array of distances
   arrJunctionsDist <- rep(NA,length(arrJunctions))
-
+  
   #Filter unique junctions
   arrJunctionsUnique <- unique(arrJunctions)
-
+  
   #Map indexes of unique to its non-unique in the original arrJunctions
   indexJunctions <- match(arrJunctions, arrJunctionsUnique)
-
+  
   #Identify junctions with multiple non-unique sequences and set its distances to 0
   indexJunctionsCounts <- table(indexJunctions)
   indexRepeated <- as.numeric(names(indexJunctionsCounts)[indexJunctionsCounts>1])
   indexRepeated <- indexJunctions%in%indexRepeated
   arrJunctionsDist[ indexRepeated ] <- rep(0,sum(indexRepeated))
   names(arrJunctionsDist) <- arrJunctions
-
+  
   #Compute distances between junctions
   numbOfUniqueJuctions <- length(arrJunctionsUnique)
   arrUniqueJunctionsDist <- rep(NA,numbOfUniqueJuctions)
@@ -118,7 +118,7 @@ getDistanceToClosest <- function(arrJunctions, subs, mut){
     arrUniqueJunctionsDist <- sapply(1:numbOfUniqueJuctions, function(i){ min(matDistance[-i,i]) })
     names(arrUniqueJunctionsDist) <- arrJunctionsUnique
   }
-
+  
   #Fill the distances for the sequences that are unique
   arrJunctionsDist[is.na(arrJunctionsDist)] <- arrUniqueJunctionsDist[indexJunctionsCounts==1]
   return(round(arrJunctionsDist,4))
@@ -144,7 +144,8 @@ getDistanceToClosest <- function(arrJunctions, subs, mut){
 #' @param   model      SHM targeting model; must be one of c("hs5f", "m3n"). See Details for further information.
 #' @param   vector     if \code{TRUE} return a numeric vector of only the distances; if \code{FALSE} return the
 #'                     entire input data.frame with a DIST_NEAREST column added.
-#'
+#' @param    numbOfCores     The number of cores to distribute the function over.
+#' 
 #' @return  If \code{vector=TRUE} returns a numeric vector of distances of each sequence to its nearest neighbor.
 #'          If \code{vector=FALSE} returns a modified \code{db} data.frame with nearest neighbor distances
 #'          in the DIST_NEAREST column.
@@ -160,54 +161,65 @@ getDistanceToClosest <- function(arrJunctions, subs, mut){
 #'
 #' @export
 distToNearest <- function(db, seq="JUNCTION", genotyped=FALSE, first=TRUE, model="hs5f",
-                          vector=FALSE) {
-	if(!is.data.frame(db)) { stop('Must submit a data frame') }
-
-	if(genotyped) {
-		v_col <- "V_CALL_GENOTYPED"
-	} else {
-		v_col <- "V_CALL"
-	}
-	j_col <- "J_CALL"
-
-	# Parse V and J Column to get gene
-	# cat("V+J Column parsing\n")
-
-	if(first) {
-		db$V <- getGene(db[,v_col])
-		db$J <- getGene(db[,j_col])
-	} else {
-	  db$V <- getGene(db[,v_col], first=FALSE)
-	  db$J <- getGene(db[,j_col], first=FALSE)
+                          vector=FALSE, numbOfCores=1) {
+  if(!is.data.frame(db)) { stop('Must submit a data frame') }
+  
+  if(genotyped) {
+    v_col <- "V_CALL_GENOTYPED"
+  } else {
+    v_col <- "V_CALL"
+  }
+  j_col <- "J_CALL"
+  
+  # Parse V and J Column to get gene
+  # cat("V+J Column parsing\n")
+  
+  if(first) {
+    db$V <- getGene(db[,v_col])
+    db$J <- getGene(db[,j_col])
+  } else {
+    db$V <- getGene(db[,v_col], first=FALSE)
+    db$J <- getGene(db[,j_col], first=FALSE)
     # Reassign V genes to most general group of genes
     for(ambig in unique(db$V[grepl(',',db$V)]))
       for(g in strsplit(ambig, split=','))
         db$V2[db$V==g] = ambig
     # Reassign J genes to most general group of genes
-	  for(ambig in unique(db$J[grepl(',',db$J)]))
-	    for(g in strsplit(ambig, split=','))
-	      db$J[db$J==g] = ambig
-	}
-
-	# Load targeting model
-	# cat("Loading Targeting Model\n")
-    model_data <- loadModel(model)
-
-	# Create new column for distance to nearest neighbor
-	db$DIST_NEAREST <- rep(NA, nrow(db))
-	db$ROW_ID <- 1:nrow(db)
-    db$L <- nchar(db[, seq])
-
-	# cat("Calculating distance to nearest neighbor\n")
-	db <- arrange(ddply(db, .(V, J, L), here(mutate),
-                        DIST_NEAREST=getDistanceToClosest(eval(parse(text=seq)),
-                                                          subs=model_data[['subs']],
-                                                          mut=model_data[['mut']])),
-                  ROW_ID)
-
-    if (vector) {
-        return(db$DIST_NEAREST)
-    } else {
-        return(db[, !(names(db) %in% c("V", "J", "L", "ROW_ID"))])
-    }
+    for(ambig in unique(db$J[grepl(',',db$J)]))
+      for(g in strsplit(ambig, split=','))
+        db$J[db$J==g] = ambig
+  }
+  
+  # Load targeting model
+  # cat("Loading Targeting Model\n")
+  model_data <- loadModel(model)
+  
+  # Create new column for distance to nearest neighbor
+  db$DIST_NEAREST <- rep(NA, nrow(db))
+  db$ROW_ID <- 1:nrow(db)
+  db$L <- nchar(db[, seq])
+  
+  # cat("Calculating distance to nearest neighbor\n")
+  runAsParallel <- FALSE
+  if(numbOfCores>1){
+    cluster <- makeCluster(numbOfCores, type = "SOCK")
+    registerDoSNOW(cluster)
+    clusterEvalQ(cluster, library(shm,alakazam))
+    runAsParallel <- TRUE
+  }
+  
+  db <- arrange(ddply(db, .(V, J, L), here(mutate),
+                      DIST_NEAREST=getDistanceToClosest(eval(parse(text=seq)),
+                                                        subs=model_data[['subs']],
+                                                        mut=model_data[['mut']]), .parallel=runAsParallel),
+                ROW_ID)
+  
+  if(runAsParallel){
+    stopCluster(cluster)
+  }
+  if (vector) {
+    return(db$DIST_NEAREST)
+  } else {
+    return(db[, !(names(db) %in% c("V", "J", "L", "ROW_ID"))])
+  }
 }
