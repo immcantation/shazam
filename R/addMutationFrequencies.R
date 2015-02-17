d060d0475397ef9cc0ecc9242522792f01dfc371
Mohamed Uduman <mohamed.uduman@gmail.com>
2015-02-17 10:37:44 -0500

    updated addMutationFrequencies.R

---
+++ R/addMutationFrequencies.R
@@ -0,0 +1,45 @@
+#' Calculate mutation frequencies
+#'
+#' \code{addMutationFrequencies} calculates the mutation frequencies.
+#'
+#' Methods.
+#'
+#' @param    db              data.frame containing sequence data.
+#' @param    sequenceColumn  The name of the sequence column.
+#' @param    germlineColumn  The name of the germline column.
+#' @param    numbOfCores     The number of cores to distribute the function over.
+#' @return   A modified \code{db} data.frame with observed mutations in the
+#'           OBSERVED_R_CDR (CDR replacement), OBSERVED_S_CDR (CDR silent),
+#'           OBSERVED_R_FWR, (FWR replacement, and OBSERVED_S_FWR (FWR silent) columns.
+#'
+#' @examples
+#' # TODO
+#' # Working example
+#'
+#' @export
+addMutationFrequencies <- function(db, sequenceColumn="SEQUENCE_GAP", germlineColumn="GERMLINE_GAP_D_MASK", numbOfCores=1)  {
+
+  numbOfSeqs <- nrow(db)
+
+  availableCores <- getNumbOfCores()
+  if(!(numbOfCores<=availableCores))numbOfCores=availableCores
+  cluster <- makeCluster(numbOfCores, type = "SOCK")
+  registerDoSNOW(cluster)
+
+  muFreq <-
+      foreach(i=icount(numbOfSeqs), .packages='shm', .combine=doparProgressBar(n=numbOfSeqs)) %dopar% {
+        #Count numb of mutations
+        inputSeq <-  db[i,sequenceColumn]
+        totalMu <- sum(countMutations(inputSeq, db[i,germlineColumn]))
+        #Get mu freq
+        seq<-substring(inputSeq,1,312)
+        seq<-gsub("[N.-]","", seq)
+        seqLen <- nchar(seq)
+        return(totalMu/seqLen)
+      }
+  cat("\n")
+  stopCluster(cluster)
+
+  db[,"MUTATION_FREQUENCY"] <- muFreq
+  return(db)
+}
