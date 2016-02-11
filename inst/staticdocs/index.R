0a3c7b38747d78b61d9cfb95d9fffaf3cdb020e7
Jason Vander Heiden <jason.vanderheiden@yale.edu>
2016-02-11 17:07:03 -0500

    Updated staticdocs index.

--- inst/staticdocs/index.R
+++ inst/staticdocs/index.R
@@ -1,16 +1,51 @@
+sd_section("Overview",
+           "Package overview",
+           c("shazam"))
+
 sd_section("Targeting models",
            "Targeting models",
-           c("createTargetingModel", "plotMutability"))
+           c("createTargetingModel", 
+             "plotMutability",
+             "createSubstitutionMatrix",
+             "createMutabilityMatrix",
+             "createTargetingMatrix",
+             "extendSubstitutionMatrix",
+             "extendMutabilityMatrix",
+             "rescaleMutability",
+             "TargetingModel-class",
+             "M1NDistance",
+             "HS1FDistance",
+             "HS5FModel",
+             "U5NModel"))
 
 sd_section("Mutational profiling",
            "Mutational profiling",
-           c("collapseByClone", "calcDBObservedMutations", "calcDBExpectedMutations"))
+           c("collapseByClone", 
+             "calcDBObservedMutations", 
+             "calcDBExpectedMutations",
+             "calcObservedMutations",
+             "calcExpectedMutations",
+             "MutationDefinition-class",
+             "RegionDefinition-class",
+             "createMutationDefinition",
+             "createRegionDefinition",
+             "MUTATION_SCHEMES",
+             "IMGT_SCHEMES"))
 
 sd_section("Selection analysis",
            "Selection analysis",
-           c("calcBaseline", "groupBaseline", "summarizeBaseline",
-             "plotBaselineDensity", "plotBaselineSummary"))
+           c("calcBaseline",
+             "groupBaseline",
+             "plotBaselineDensity",
+             "plotBaselineSummary",
+             "createBaseline",
+             "getBaselineStats",
+             "summarizeBaseline",
+             "Baseline-class"))
 
 sd_section("Distance profiling",
            "Distance profiling",
-           c("distToNearest", "calcTargetingDistance"))
+           c("distToNearest", 
+             "calcTargetingDistance",
+             "writeTargetingDistance"))
+
