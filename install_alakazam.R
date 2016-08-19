ca46e9147820d1a801d18529c04beecc6f16c982
Susanna Marquez <susanna.marquez@yale.edu>
2016-08-19 11:23:14 -0400

    If version .999, run tests using the most recent version of alakazam (Bitbucket)

--- install_alakazam.R
+++ install_alakazam.R
@@ -1,20 +1,22 @@
 library(devtools)
 library(versions)
 
+shazam_version <- read.dcf("DESCRIPTION", fields = "Version")
+shazam_devel <- length(grep("\\.999$",shazam_version)) > 0
+
 d <- read.dcf("DESCRIPTION", fields = "Imports")
 d <- sub("^\\n","",d)
 imports <- strsplit(d,",\n")[[1]]
 alakazam <- imports[grep("alakazam", imports)]
 
 required_version <- gsub(".*\\([^0-9.]*(.*)\\)$", "\\1", alakazam)
-devel_999 <- length(grep("\\.999$",required_version)) > 0
+alakazam_devel <- length(grep("\\.999$",required_version)) > 0
 cran_versions <- available.versions("alakazam")
 cran_versions <- cran_versions$alakazam$version
 
-
-if (required_version %in% cran_versions) {
+if (!shazam_devel & !alakazam_devel & (required_version %in% cran_versions)) {
     install.versions("alakazam",required_version)
 } else {
-    if (!devel_999) { warning(paste0(required_version," not found in CRAN. Used Bitbucket instead.")) }
+    if (!alakazam_devel) { warning(paste0(required_version," not found in CRAN. Used Bitbucket instead.")) }
     install_bitbucket("kleinstein/alakazam@default")
 }
