0a6262ca8fde0680177ba4b92c361d0ad41f8f90
Susanna Marquez <susanna.marquez@yale.edu>
2017-05-19 20:13:52 -0400

    Pseudo fix for calcBaseline. TODO actual fix.

--- R/Baseline.R
+++ R/Baseline.R
@@ -407,7 +407,7 @@ calcBaseline <- function(db,
     }
     
     # Convert sequence columns to uppercase
-    db <- toupperColumns(db, c(sequenceColumn, germlineColumn))
+    db <- data.frame(toupperColumns(db, c(sequenceColumn, germlineColumn)))
     
     # Ensure that the nproc does not exceed the number of cores/CPUs available
     nproc <- min(nproc, getnproc())
