3a5af607038c28cc5d3ad8cc8647d4f6cbf5c251
Noah <noah.yann.lee@yale.edu>
2021-11-02 19:08:59 -0400

    clean ConvertNumbering & tests
    removed old IMGTToKabat

--- R/ConvertNumbering.R
+++ R/ConvertNumbering.R
@@ -7,36 +7,39 @@
 #' with Gaps (unoccupied positions) shown by "G" and Asterisks (*) shown by "S": 
 #' arbitrary mappings (multiple possible "to" values) represented with "NA"
 #'
-#' @param   locus   string indicating heavy ("IGH") or light chains ("IGK" or "IGL)
-#' @param   from    string indicating numbering system to convert to ("IMGT" or "KABAT")
-#' @param   to      string indicating original numbering system ("IMGT" or "KABAT")
-#' @param   x       vector of strings representing original numbering
+#' @param   locus     string indicating heavy ("IGH") or light chains ("IGK" or "IGL)
+#' @param   from      string indicating numbering system to convert to ("IMGT" or "KABAT")
+#' @param   to        string indicating original numbering system ("IMGT" or "KABAT")
+#' @param   calls     vector of strings representing original numbering
 #' @return  A vector of string indicating the corresponding numbering
 #' 
 #' @examples
 #' convertNumbering("IGH", "IMGT", "KABAT", c("51", "23", "110"))
 #' convertNumbering("IGH", "KABAT", "IMGT", c("51", "23", "G"))
-# TODO: @export
-convertNumbering <- function(locus, from, to, arrayOfSeqs) {
-  from_map <- pull(CONVERT_NUM_REF, paste(locus, from, sep='_')) 
-  to_map <- pull(CONVERT_NUM_REF, paste(locus, to, sep='_')) 
-
-  # Recode has issues with alternative types
-  if (!all(arrayOfSeqs %in% from_map)) {
-    stop(paste("Formatting of following characters does not match reference: ", 
-               toString(arrayOfSeqs[!arrayOfSeqs %in% from_map])))
-  }
-  
-  out_arrayOfSeqs <- dplyr::recode(as.character(arrayOfSeqs), !!! setNames(to_map, from_map))
-  
-  # Separate check for ambiguous values to convert to NA, not arbitrary
-  from_counts <- table(from_map)
-  
-  for(i in arrayOfSeqs){
-    if (from_counts[i] > 1) {
-      out_arrayOfSeqs[which(arrayOfSeqs==i)[1]] <- "NA"
+#' @export
+convertNumbering <- function(locus, from, to, calls) {
+    # Generate mapping from references
+    from_map <- pull(CONVERT_NUM_REF, paste(locus, from, sep='_')) 
+    to_map <- pull(CONVERT_NUM_REF, paste(locus, to, sep='_')) 
+    
+    # Check for compatibility of reference with input calls
+    if (!all(calls %in% from_map)) {
+        stop(paste("Formatting of following characters does not match reference: ", 
+            toString(calls[!calls %in% from_map])))
     }
-  }
-  
-  return(as.character(out_arrayOfSeqs))
+    
+    out_calls <- dplyr::recode(as.character(calls), 
+                                     !!! setNames(to_map, from_map))
+    
+    # Separate check for ambiguous values to convert to NA, not arbitrary call
+    from_counts <- table(from_map)
+    
+    # Generate output
+    for(i in calls){
+        if (from_counts[i] > 1) {
+            out_calls[which(calls==i)] <- "NA"
+        }
+    }
+    
+    return(as.character(out_calls))
 }
--- tests/testthat/test_ConvertNumbering.R
+++ tests/testthat/test_ConvertNumbering.R
@@ -1,29 +1,36 @@
 #### ConvertNumbering ####
 test_that("Ensure numberings are consistent and stable", {
-    ## Reproduce example
+    # Generate assortment of calls
     imgt_seqs <- c("51", "23", "31", "18", "4", "90", "58", "59", "3")
     kabat_seqs <-c("*55", "82B", "91", "23")
+    # Specific calls with known incompatibility across numbering schemes
     known_miss_imgt <- c("73", "110", "114", "115")
-    known_arb_kabat <- c("G", "*")
+    known_arb_kabat <- c("G", "*", "G")
     
-    # Should be able to retain original numberings
-    expect_equal(convertNumbering("IGH", "KABAT", "KABAT", kabat_seqs), kabat_seqs)
-    expect_equal(convertNumbering("IGH", "IMGT", "IMGT", imgt_seqs), imgt_seqs)
-    
+    # Expected calls after converting
     kabat_imgt_expected <- c("63", "93", "103", "24")
     imgt_kabat_expected <- c("46", "22", "*30", "17", "4", "81", "*52A", "*52B", "3")
     known_miss_expected <- c("G", "G", "G", "G")
-    known_arb_expected <- c("NA", "NA")
+    known_arb_expected <- c("NA", "NA", "NA")
+    
+    # Maintain original numbering check
+    expect_equal(convertNumbering("IGH", "KABAT", "KABAT", kabat_seqs), kabat_seqs)
+    expect_equal(convertNumbering("IGH", "IMGT", "IMGT", imgt_seqs), imgt_seqs)
     
     # Expected conversions
-    expect_equal(convertNumbering("IGH", "KABAT", "IMGT", kabat_seqs),kabat_imgt_expected)
-    expect_equal(convertNumbering("IGH", "IMGT", "KABAT", imgt_seqs),imgt_kabat_expected)
-    expect_equal(convertNumbering("IGH", "IMGT", "KABAT", known_miss_imgt),known_miss_expected)
-    expect_equal(convertNumbering("IGH", "KABAT", "IMGT", known_arb_kabat),known_arb_expected)
+    expect_equal(convertNumbering("IGH", "KABAT", "IMGT", kabat_seqs),
+                 kabat_imgt_expected)
+    expect_equal(convertNumbering("IGH", "IMGT", "KABAT", imgt_seqs),
+                 imgt_kabat_expected)
+    expect_equal(convertNumbering("IGH", "IMGT", "KABAT", known_miss_imgt),
+                 known_miss_expected)
+    expect_equal(convertNumbering("IGH", "KABAT", "IMGT", known_arb_kabat),
+                 known_arb_expected)
     
-    # Missing values or incorrect formats
+    # Ensure unknown values are flagged
     issue_seqs <- c("55", "23", "52", "82D")
     expect_error(convertNumbering("IGH", "KABAT", "IMGT", issue_seqs), 
-                 "Formatting of following characters does not match reference:  55, 82D", fixed=TRUE)
+                 "Formatting of following characters does not match reference:  55, 82D", 
+                 fixed=TRUE)
 })
 
